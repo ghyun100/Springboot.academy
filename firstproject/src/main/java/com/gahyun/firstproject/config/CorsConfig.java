36c83410ce655fe07fb1d9bc91dcf42f04d8e310
ghyun100 <bgh3508@naver.com>
2023-04-21 12:52:28 +0900

    2023.04.21

---
+++ firstproject/src/main/java/com/gahyun/firstproject/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.gahyun.firstproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry
+        .addMapping("/**")
+        .allowedMethods("*")
+        .allowedOrigins("*");
+    
+    
+
+    }
+
+    
+}
